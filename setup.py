ba887b4b01da24f091db9840c39fe4e15c48c390
hvanphucs <47340777+hvanphucs@users.noreply.github.com>
2023-09-07 10:52:43 +0700

    build

--- setup.py
+++ setup.py
@@ -1,34 +1,27 @@
-
 import setuptools
 
-with open("README.md", "r", encoding = "utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-
 setuptools.setup(
-    name = "sckinetics",
-    version = "1.0",
-    author = "author",
-    author_email = "author@example.com",
-    description = "short package description",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/Lalit-shaktawat/scKINETICS",
-    project_urls = {
-        "Bug Tracker": "package issues URL",
+    name="sckinetics",
+    version="1.0",
+    author="bioturing",
+    author_email="info@bioturing.com",
+    description="scKINETICS (Key regulatory Interaction NETwork for Inferring Cell Speed)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bioturing-datalake/sckinetics-py",
+    project_urls={
+        "Bug Tracker": "https://github.com/bioturing-datalake/sckinetics-py/issues",
     },
-    classifiers = [
-        "Programming Language :: Python :: 3",
- 	"Programming Language :: Python :: 3",
-    	"Programming Language :: Python :: 3.7",
-    	"Programming Language :: Python :: 3.8",
-    	"Programming Language :: Python :: 3.9",
-    	"Programming Language :: Python :: 3.10",
-    	"Programming Language :: Python :: 3.11",
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
     ],
-    package_dir = {"": "src"},
-    packages = setuptools.find_packages(where="src"),
-    python_requires = ">=3.9"
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7,<=3.9"
 )
